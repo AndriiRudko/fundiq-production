43ad9261b3b7e4077f3509ed77bdbcb920e65039
AndriiRudko <byandriyrudko@gmail.com>
2023-04-11 16:43:39 +0300

    task 1 complete

--- src/components/TaskOne/TaskOne.jsx
+++ src/components/TaskOne/TaskOne.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
 const TaskOne = () => {
+
+ 
+  const [counter, setCounter] = useState(0);
+
+  const plusCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const minysCounter = () => {
+    setCounter(counter - 1)
+  }
+
+
   return (
     <div>
-      <h2>TaskOne</h2>
-      <Outlet />
+    <p>Поточне число: {counter}</p>
+    <button onClick={plusCounter}>+</button>
+    <button onClick={minysCounter}>-</button>
+    <Outlet />
     </div>
+
   )
 }
 
--- src/components/TaskTwo/TaskTwo.jsx
+++ src/components/TaskTwo/TaskTwo.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom';
 
 const TaskTwo = () => {
-  return (
-    <>
-    <div>TaskTwo</div>
-    <Outlet />
-    </>
+return (
 
-  )
-}
+  <div>
+      <h2>TaskTwo</h2>
+      <Outlet />
+    </div>
+)
+
+  }
 
 export default TaskTwo;
